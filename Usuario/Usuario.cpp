9aea836e67c74b6c2a8f908ad8ed5c6d0d634333
jayjay <167156089+jjnootfound@users.noreply.github.com>
2025-12-04 18:36:22 -0600

    feat: cpp file, added methods

--- Usuario/Usuario.cpp
+++ Usuario/Usuario.cpp
@@ -1,1 +1,40 @@
 #include "Usuario.h"
+
+Usuario::Usuario() {
+    id = 0;
+    nombre = "";
+    kmAcumulados = 0;
+}
+
+Usuario::Usuario(int id, string nombre, int kmAcumulados) {
+    this->id = id;
+    this->nombre = nombre;
+    this->kmAcumulados = kmAcumulados;
+}
+
+int Usuario::getId() {
+    return id;
+}
+string Usuario::getNombre() {
+    return nombre;
+}
+
+int Usuario::getKmAcumulados() {
+    return kmAcumulados;
+}
+
+void Usuario::setId(int id) {
+    this->id = id;
+}
+
+void Usuario::setNombre(string nombre) {
+    this->nombre = nombre;
+}
+
+void Usuario::setKmAcumulados(int kmAcumulados) {
+    this->kmAcumulados = kmAcumulados;
+}
+
+void Usuario::agregarKilometros(int km) {
+    kmAcumulados += km;
+}
