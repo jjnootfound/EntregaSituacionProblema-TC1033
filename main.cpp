53e8bbbcccc221840057f481c3446c5b5c339636
jayjay <167156089+jjnootfound@users.noreply.github.com>
2025-12-04 21:07:27 -0600

    feat: main file (war is over!!)(Testing is missing)

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "Sistema.h"
+
+int main() {
+    Sistema sistema;
+    sistema.mostrarMenu();
+    return 0;
+}
+
+/*
+Código realizado por:
+Alexhia Sophia Pérez Escobar  
+Jorlen del Carmen Moreno Soria 
+
+pd. ve el archivo "welcome.cpp" para un mensaje especial c:
+*/
